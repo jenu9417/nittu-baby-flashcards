59efe107115e665c4266cb11f78a924450024e38
Janardhanan V S <jenu9417@gmail.com>
2025-06-15 12:25:41 +0530

    [NittuBabyFlashCards]-Fixing stability issues in index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
+if (__DEV__) {
+    const defaultHandler = ErrorUtils.getGlobalHandler?.();
+    ErrorUtils.setGlobalHandler((error, isFatal) => {
+        console.error('Global App Error:', error);
+        if (defaultHandler) defaultHandler(error, isFatal);
+    });
+}
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
