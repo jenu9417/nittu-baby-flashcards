e83e9d283ec7c6dfaeb7ca632cf919ed68129991
Janardhanan V S <jenu9417@gmail.com>
2025-06-18 16:32:49 +0530

    [NittuBabyFlashCards]-Removing safeareaprovider for better fullscreen experience

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { LogBox } from 'react-native';
 
 import HomeScreen from './screens/HomeScreen';
@@ -18,15 +17,13 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Flashcard" component={FlashcardScreen} />
-          <Stack.Screen name="CustomPlaylist" component={CustomPlaylistScreen} />
-          <Stack.Screen name="Settings" component={SettingsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Flashcard" component={FlashcardScreen} />
+        <Stack.Screen name="CustomPlaylist" component={CustomPlaylistScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
